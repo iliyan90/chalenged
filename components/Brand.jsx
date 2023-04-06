5f0c3e199ea8e6507ff056ff0df5408112df0a0f
iliyan90 <74927065+iliyan90@users.noreply.github.com>
2023-04-06 08:34:50 +0300

    Delete components directory

--- components/Brand.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Brand = () => {
-  return (
-    <div style={{textAlign:'start', fontSize:'24px',padding:'20px 0'}}><h3>GitRemoteJobs</h3></div>
-  )
-}
-
-export default Brand
